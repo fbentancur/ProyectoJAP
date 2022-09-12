65678a36b912d071a0059cdcd08592ee731ab4c4
fbentancur <soyfacundo77@gmail.com>
2022-09-12 18:52:28 -0300

    Agregado de funcionalidad:Mostrar comentarios y estrellas, agregado interfaz para comentar

--- js/product-info.js
+++ js/product-info.js
@@ -1,11 +1,12 @@
 let infoContainer = document.getElementById("contenedorDeInformacionDeProducto")
+let commentContainer = document.getElementById("contenedor-de-comentarios")
+let botonComentario = document.getElementById("enviar-comentario")
 
 fetch(PRODUCT_INFO_URL+localStorage.getItem('productId')+EXT_TYPE)
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-    let temporal =""
-    temporal+=
+    .then(response => response.json())
+    .then(data => {
+        let temporalInfo =""
+        temporalInfo+=
         `<div>
             <h1>${data.name}</h1>
             <hr>
@@ -20,13 +21,41 @@ fetch(PRODUCT_INFO_URL+localStorage.getItem('productId')+EXT_TYPE)
             
         </div> 
         <h2>Imagenes ilustrativas:</h2>`
-        temporal+=`<div id="imagenesDeProductos">`
+        temporalInfo+=`<div id="imagenesDeProductos">`
         for(let imagen of data.images){
-          temporal+= `<div class="divImagenIndividual"><img src="${imagen}" class="product_image"></div>`
+        temporalInfo+= `<div class="divImagenIndividual"><img src="${imagen}" class="product_image"></div>`
             
             
         }
-        temporal+=`</div>`
-        infoContainer.innerHTML = temporal
+        temporalInfo+=`</div>`
+        infoContainer.innerHTML = temporalInfo
 })
+
+fetch(PRODUCT_INFO_COMMENTS_URL+localStorage.getItem('productId')+EXT_TYPE)
+    .then(response => response.json())
+    .then(dataComment => {
+        console.log(dataComment)
+        let temporalComments = ""
+        temporalComments+=`<div class="caja_de_comentarios">`
+        for(let comentarioData of dataComment){
+            let cantidadDeEstrellas = ""
+            for(let i =0; i < comentarioData.score; i++){
+                cantidadDeEstrellas+=`<span class="fa fa-star checked"></span>`
+            }
+            for(let i =0; i < 5-comentarioData.score; i++){
+                cantidadDeEstrellas+=`<span class="fa fa-star"></span>`
+            }
+            temporalComments +=`
+            <div class="comentario">
+                <span><b>${comentarioData.user}</b> ${comentarioData.dateTime} ${cantidadDeEstrellas}</span>
+                <p>${comentarioData.description}</p>
+            </div>`
+        }
+        temporalComments+=`</div>`
+        commentContainer.innerHTML=temporalComments
+    })
+
+    botonComentario.addEventListener('click', function(){
+        location.reload();
+    } )
 
