59e4ec7f7f87218f4ddae113bfd00652d745a331
fbentancur <soyfacundo77@gmail.com>
2022-10-26 15:20:39 -0300

    Entrega 6 finalizada(desordenado)

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,12 @@
 let cartContent = document.getElementById("cart-container")
 
-fetch(CART_INFO_URL+25801+EXT_TYPE)
+fetch(CART_INFO_URL + 25801 + EXT_TYPE)
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        for(let content of data.articles){
-            let contentForInner=""
-            contentForInner+=`
+        for (let content of data.articles) {
+            let contentForInner = ""
+            contentForInner += `
     <div class="table-div">
         <table class="product-table">
             <thead>
@@ -24,7 +24,7 @@ fetch(CART_INFO_URL+25801+EXT_TYPE)
                     <td>${content.name}</td>
                     <td>${content.unitCost}</td>
                     <td><input type="number" id="cantidad-productos+${content.id}" class="input-cantidad" value="${content.count}" min="1"></td>
-                    <td id="precio-total">${content.unitCost*content.count}</td>
+                    <td id="precio-total">${content.unitCost * content.count}</td>
                 </tr>
             </tbody>
         </table>
@@ -33,93 +33,96 @@ fetch(CART_INFO_URL+25801+EXT_TYPE)
 
             let tarjeta = document.getElementById('tarj-number')
             let cvu = document.getElementById('cvu')
-            let vencimiento =document.getElementById('vencimiento')
-            let cuenta= document.getElementById("numero-cuenta")
+            let vencimiento = document.getElementById('vencimiento')
+            let cuenta = document.getElementById("numero-cuenta")
             let boton = document.getElementById('boton-modal')
 
-            document.getElementById("credito").addEventListener("click",()=>{
-                document.getElementById("numero-cuenta").disabled=true
-                tarjeta.disabled=false
-                cvu.disabled=false
-                vencimiento.disabled=false
-                boton.innerHTML="Forma de pago : Tarjeta"
-            })
-            
-            document.getElementById('transferencia').addEventListener("click", ()=>{
-                tarjeta.disabled=true
-                cvu.disabled=true
-                vencimiento.disabled=true
-                cuenta.disabled=false
-                boton.innerHTML="Forma de pago : Transferencia"
+            document.getElementById("credito").addEventListener("click", () => {
+                document.getElementById("numero-cuenta").disabled = true
+                tarjeta.disabled = false
+                cvu.disabled = false
+                vencimiento.disabled = false
+                boton.innerHTML = "Forma de pago : Tarjeta"
             })
 
-            cartContent.innerHTML+=contentForInner
+            document.getElementById('transferencia').addEventListener("click", () => {
+                tarjeta.disabled = true
+                cvu.disabled = true
+                vencimiento.disabled = true
+                cuenta.disabled = false
+                boton.innerHTML = "Forma de pago : Transferencia"
+            })
+
+            cartContent.innerHTML += contentForInner
 
             let cantidad = document.getElementById(`cantidad-productos+${content.id}`)
             let subtotal = document.getElementById('subtotal-price')
-            let envioCost =document.getElementById('envio-price')
+            let envioCost = document.getElementById('envio-price')
             let precioTotal = document.getElementById('total-price')
             let tiposDeEnvio = document.querySelectorAll("[name='tipo-de-envio']")
             let coste = document.getElementById("precio-total")
-            subtotal.innerHTML=`<p>${content.currency} ${content.unitCost}</p>`
-            envioCost.innerHTML=`<p>${content.currency} 0</p>`
-            precioTotal.innerHTML=`<p>${content.currency} ${content.unitCost*cantidad.value}</p>`
-            
+            subtotal.innerHTML = `<p>${content.currency} ${content.unitCost}</p>`
+            envioCost.innerHTML = `<p>${content.currency} 0 </p>`
+            precioTotal.innerHTML = `<p>${content.currency} ${content.unitCost * cantidad.value}</p>`
 
-            cantidad.addEventListener("change",()=>{
-                precioTotal.innerHTML=`<p>${content.currency} ${content.unitCost*cantidad.value}</p>`
+
+            cantidad.addEventListener("change", () => {
+                precioTotal.innerHTML = `<p>${content.currency} ${content.unitCost * cantidad.value}</p>`
                 rellenarCostes()
             })
 
-            for(let i =0; i< tiposDeEnvio.length; i++){
+            for (let i = 0; i < tiposDeEnvio.length; i++) {
                 console.log(tiposDeEnvio[i])
-                tiposDeEnvio[i].addEventListener("click",()=>{
+                tiposDeEnvio[i].addEventListener("click", () => {
                     rellenarCostes()
                 })
-            } 
-            
+            }
 
-        function rellenarCostes(){
-            coste.innerHTML=`${cantidad.value * content.unitCost}`
-            subtotal.innerHTML=`<p>${content.currency} ${cantidad.value * content.unitCost}</p>`
-            
-            for(let tipoDeEnvio of tiposDeEnvio){
-                if(tipoDeEnvio.checked && tipoDeEnvio.id =="premium"){
-                    envioCost.innerHTML=`<p>${content.currency} ${(15*(cantidad.value * content.unitCost))/100}</p>`
-                    precioTotal.innerHTML=`<p>${content.currency} ${((15*(cantidad.value * content.unitCost))/100)+ (cantidad.value * content.unitCost)}</p>`
-                }
-                    else if(tipoDeEnvio.checked && tipoDeEnvio.id =="express"){
-                        envioCost.innerHTML=`<p>${content.currency} ${(7*(cantidad.value * content.unitCost))/100}</p>`
+
+            function rellenarCostes() {
+                coste.innerHTML = `${cantidad.value * content.unitCost}`
+                subtotal.innerHTML = `<p>${content.currency} ${cantidad.value * content.unitCost}</p>`
+
+                for (let tipoDeEnvio of tiposDeEnvio) {
+                    if (tipoDeEnvio.checked && tipoDeEnvio.id == "premium") {
+                        envioCost.innerHTML = `<p>${content.currency} ${(15 * (cantidad.value * content.unitCost)) / 100}</p>`
+                        precioTotal.innerHTML = `<p>${content.currency} ${((15 * (cantidad.value * content.unitCost)) / 100) + (cantidad.value * content.unitCost)}</p>`
                     }
-                        else if(tipoDeEnvio.checked && tipoDeEnvio.id =="standard"){
-                            envioCost.innerHTML=`<p>${content.currency} ${(5*(cantidad.value * content.unitCost))/100}</p>`
-                        }
+                    else if (tipoDeEnvio.checked && tipoDeEnvio.id == "express") {
+                        envioCost.innerHTML = `<p>${content.currency} ${(7 * (cantidad.value * content.unitCost)) / 100}</p>`
+                    }
+                    else if (tipoDeEnvio.checked && tipoDeEnvio.id == "standard") {
+                        envioCost.innerHTML = `<p>${content.currency} ${(5 * (cantidad.value * content.unitCost)) / 100}</p>`
+                    }
+                }
             }
+
+            let botonComprar = document.getElementById('btn-comprar')
+            let form = document.getElementById('formulario')
+            let error = document.getElementById('error')
+            let botonModal = document.getElementById('boton-modal')
+
+            botonModal.addEventListener("click", () => {
+                error.innerHTML = ""
+            })
+
+            botonComprar.addEventListener('click', () => {
+                if (!form.checkValidity()) {
+                    error.innerHTML = `<div class="container">
+                <div class="alert alert-danger text-center" role="alert">
+                  <h4 class="alert-heading">Elija un metodo de pago</h4>
+                </div>
+              </div>`
+                }
+            })
+
+
+
         }
-        let botonComprar = document.getElementById('btn-comprar')
-        let form = document.getElementById('form-modal')
-        let botonCheckModal = document.getElementById('check-modal')
-        
-        form.addEventListener('submit',(e)=>{
-            if(!form.checkValidity()){
-                e.preventDefault()
-                e.stopPropagation()
-            }
-            form.classList.add("was-validated")
-        },false)
 
-        botonCheckModal.addEventListener('click',()=>{
-            let event = new Event("click",{bubbles: true, cancelable:true})
-            form.dispatchEvent(event)
-            alert("puto")
-        })
-        
-    }
-        
-})
+    })
 
 
 
-    
-    
-    
+
+
